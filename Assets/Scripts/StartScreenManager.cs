577e8f5dc92e6e66a8b8176801f05f8c6b62c823
Luke.Batchelor <batchelor.luke+git@gmail.com>
2020-02-23 12:21:48 +1100

    Adds start screen

---
+++ Assets/Scripts/StartScreenManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class StartScreenManager : MonoBehaviour
+{
+  public GameObject startPanel;
+  public GameObject optionsPanel;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  public void CloseGame()
+  {
+#if UNITY_EDITOR
+    // Application.Quit() does not work in the editor so
+    // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+    UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+  }
+
+  public void OptionsMenuBackButtonPressed()
+  {
+    optionsPanel.SetActive(false);
+    startPanel.SetActive(true);
+  }
+
+  public void MainScreenOptionsButtonPressed()
+  {
+    optionsPanel.SetActive(true);
+    startPanel.SetActive(false);
+  }
+
+  public void startGame() {
+    SceneManager.LoadScene("GameScene");
+  }
+}
