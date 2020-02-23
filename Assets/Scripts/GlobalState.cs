842b5af15d2307b19c30802b8c4b4794b5c203f2
Luke.Batchelor <batchelor.luke+git@gmail.com>
2020-02-23 21:47:33 +1100

    Fixes borked colors

--- Assets/Scripts/GlobalState.cs
+++ Assets/Scripts/GlobalState.cs
@@ -8,7 +8,13 @@ public static class GlobalState
     public static int CurrentScore = 0;
     public static int LevelsUnlocked = 1;
 
-    public static Color[] colors;
+    public static Color[] colors = new Color[] {
+      new Color(0.776f, 0.867f, 0.322f),
+      new Color(0.984f, 0.722f, 0.098f),
+      new Color(0.875f, 0.239f, 0.369f),
+      new Color(0.427f, 0.290f, 0.463f),
+      new Color(0.212f, 0.592f, 0.557f)
+    };
 
     
     public static void incrementLevelsUnlocked() {
--- Assets/Scripts/StartScreenManager.cs
+++ Assets/Scripts/StartScreenManager.cs
@@ -12,7 +12,7 @@ public class StartScreenManager : MonoBehaviour
   // Start is called before the first frame update
   void Start()
   {
-
+            Debug.Log("Color" + Color.black);
   }
 
   // Update is called once per frame
