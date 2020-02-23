427d88b135a35b5477d3275ff724ecd1cd889c31
Josh Batchelor <josh_batch@live.com.au>
2020-02-23 15:23:08 +1100

    mend

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStateManager : MonoBehaviour
+{
+    private static GameStateManager _instance;
+    public static GameStateManager Instance { get { return _instance; } }
+
+    [Header("Global State")]
+    public bool CanInteract = true;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+}
